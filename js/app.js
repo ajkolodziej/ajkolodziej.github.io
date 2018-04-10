7400f4195ea7dbeb09379c74d4b9cb2aa944b0c4
ajkolodziej <ajkolodziej@gmail.com>
2018-04-10 10:36:58 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   
-  navigator.serviceWorker.register('../../service-worker.js', {scope: '/'})
+  navigator.serviceWorker.register('../../service-worker.js', {scope: './'})
   .then(function(reg) {
     if(reg.installing) {
       console.log('Service worker installing');
