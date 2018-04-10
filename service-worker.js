301695225c2563f1f1dcf1fa5ae22ed058709cbe
ajkolodziej <ajkolodziej@gmail.com>
2018-04-10 10:15:48 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,14 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'app-cache';
 
+const urlsToCache = [
+  '/index.html',
+  '/',
+  'css/style.css',
+  'js/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+    return cache.addAll(urlsToCache);
+  }));
 });
