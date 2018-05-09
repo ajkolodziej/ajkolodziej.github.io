f6bd3c409c2a11b9926e12d9aa74170a36797401
ajkolodziej <ajkolodziej@gmail.com>
2018-05-09 18:49:12 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
+function getLocation() {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geo Location not supported by browser");
+  }
+}
 
+//function that retrieves the position
+function showPosition(position) {
+  var location = {
+    longitude: position.coords.longitude,
+    latitude: position.coords.latitude
+  }
+  console.log(location)
+}
+
+getLocation();
