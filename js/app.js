cbc14ba13529edc57e539d8e3297f09436bd0c6e
ajkolodziej <ajkolodziej@gmail.com>
2018-05-09 19:00:05 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('../service-worker.js').then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+
 function getLocation() {
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
